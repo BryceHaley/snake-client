c6a4fcc4f296356af3b1a0d3c17a8171c9c09e26
Bryce Haley <bhaley91@gmail.com>
2022-08-11 10:59:24 -0400

    snake now apears with name SNK

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+/**Module to handle connecting to server */
+
 const net = require("net");
 
 // establishes a connection with the game server
@@ -11,7 +13,8 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log('Starting Snek')
+    console.log('Starting Snek');
+    conn.write("Name: SNK"); //name of snake played in game
   });
 
   conn.on("data", (data) => {
