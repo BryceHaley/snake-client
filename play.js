16b32e986f634eb68048cabd7d8a8e847423b7e6
Bryce Haley <bhaley91@gmail.com>
2022-08-11 10:49:46 -0400

    moved connect function to client.js

--- play.js
+++ play.js
@@ -1,25 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log('Starting Snek')
-  });
-
-  conn.on("data", (data) => {
-    console.log(data);
-  })
-
-  return conn;
-};
+const connect  = require('./client.js');
 
 
 
